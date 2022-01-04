467d4b812a5ae2132e9437701b36f2d2af65b4e1
Lưu Tiến Phát <luutienphat@gmail.com>
2022-01-05 00:04:50 +0700

    Add CRUD methods to modify records in table "KeSach"

--- src/Entities/KeSach.java
+++ src/Entities/KeSach.java
@@ -4,7 +4,13 @@
  */
 package Entities;
 
+import Form.ThuKho.DBAccess;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.FetchType;
@@ -18,28 +24,28 @@ import javax.persistence.Table;
  *
  * @author Phat
  */
-@Table(name = "KeSach")
+//@Table(name = "KeSach")
 public class KeSach {
 
     private String maKe;
     private String tenKe;
-    private String thongKe;
-
-    @ManyToOne()
-    @JoinColumn(name = "maKhu")
+    private String thongTin;
     private Khu khu;
+    private List<NganKeSach> cacNganKeSach = new ArrayList<>();
 
-    @OneToMany(mappedBy = "keSach", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private List<NganKeSach> cacNganKeSach;
-
-    public List<NganKeSach> getCacNganKeSach() {
-        return cacNganKeSach;
-    }
-
-    public void setCacNganKeSach(List<NganKeSach> cacNganKeSach) {
-        this.cacNganKeSach = cacNganKeSach;
-    }
-
+//    @ManyToOne()
+//    @JoinColumn(name = "maKhu")
+//    private Khu keSach;
+//
+//    @OneToMany(mappedBy = "keSach", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+//    private List<NganKeSach> cacNganKeSach;
+//    public List<NganKeSach> getCacNganKeSach() {
+//        return cacNganKeSach;
+//    }
+//
+//    public void setCacNganKeSach(List<NganKeSach> cacNganKeSach) {
+//        this.cacNganKeSach = cacNganKeSach;
+//    }
     public String getMaKe() {
         return maKe;
     }
@@ -56,12 +62,12 @@ public class KeSach {
         this.tenKe = tenKe;
     }
 
-    public String getThongKe() {
-        return thongKe;
+    public String getThongTin() {
+        return thongTin;
     }
 
-    public void setThongKe(String thongKe) {
-        this.thongKe = thongKe;
+    public void setThongTin(String thongTin) {
+        this.thongTin = thongTin;
     }
 
     public Khu getKhu() {
@@ -71,5 +77,101 @@ public class KeSach {
     public void setKhu(Khu khu) {
         this.khu = khu;
     }
+    
+    
 
+    public static List<KeSach> getList() {
+        List<KeSach> cacKeSach = new ArrayList<>();
+
+        try {
+            String query = "SELECT * FROM KeSach";
+            DBAccess dba = new DBAccess();
+            ResultSet rs = dba.Query(query);
+            while (rs.next()) {
+                KeSach keSach = new KeSach();
+                keSach.setMaKe(rs.getNString("maKe"));
+                keSach.setTenKe(rs.getNString("tenKe"));
+                keSach.setThongTin(rs.getNString("thongTin"));
+
+                Khu khu = Khu.retrieve(rs.getNString("maKhu"));
+                keSach.setKhu(khu);
+
+                cacKeSach.add(keSach);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Khu.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return cacKeSach;
+    }
+
+    public static List<KeSach> search(String keyword) {
+        List<KeSach> cacKeSach = new ArrayList<>();
+
+        try {
+            String query = "SELECT * FROM KeSach WHERE maKe LIKE N'%" + keyword + "%' OR tenKe LIKE N'%" + keyword + "%' OR thongTin LIKE N'%" + keyword + "%' OR maKhu LIKE N'%" + keyword + "%'";
+            DBAccess dba = new DBAccess();
+            ResultSet rs = dba.Query(query);
+            while (rs.next()) {
+                KeSach keSach = new KeSach();
+                keSach.setMaKe(rs.getNString("maKe"));
+                keSach.setTenKe(rs.getNString("tenKe"));
+                keSach.setThongTin(rs.getNString("thongTin"));
+
+                Khu khu = Khu.retrieve(rs.getNString("maKhu"));
+                keSach.setKhu(khu);
+
+                cacKeSach.add(keSach);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Khu.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return cacKeSach;
+    }
+
+    public static KeSach retrieve(String maKe) {
+        KeSach keSach = new KeSach();
+
+        try {
+            String query = "SELECT * FROM KeSach WHERE maKe = '" + maKe + "'";
+            DBAccess dba = new DBAccess();
+            ResultSet rs = dba.Query(query);
+            if (rs.next()) {
+                keSach.setMaKe(rs.getNString("maKe"));
+                keSach.setTenKe(rs.getNString("tenKe"));
+                keSach.setThongTin(rs.getNString("moTa"));
+
+                Khu khu = Khu.retrieve(rs.getNString("maKhu"));
+                keSach.setKhu(khu);
+                
+                
+            }
+            return keSach;
+        } catch (SQLException ex) {
+            Logger.getLogger(KeSach.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
+    public static boolean insert(String maKe, String tenKe, String thongTin, String maKhu) {
+        String query = "INSERT INTO KeSach VALUES(N'" + maKe + "', N'" + tenKe + "', N'" + thongTin + "', N'" + maKhu + "')";
+        DBAccess dba = new DBAccess();
+        boolean i = dba.Update(query);
+        return i;
+    }
+
+    public static boolean update(String maKe, String tenKe, String thongTin, String maKhu) {
+        String query = "UPDATE KeSach SET tenKe = N'" + tenKe + "', moTa = N'" + thongTin + "', tang = N'" + maKhu + "' WHERE maKe = N'" + maKe + "'";
+        DBAccess dba = new DBAccess();
+        boolean i = dba.Update(query);
+        return i;
+    }
+
+    public static boolean delete(String maKe) {
+        String query = "DELETE FROM KeSach WHERE maKe = N'" + maKe + "'";
+        DBAccess dba = new DBAccess();
+        boolean i = dba.Update(query);
+        return i;
+    }
 }
