769f57a0a73499d00782067e4747b570b3e74e94
Lưu Tiến Phát <luutienphat@gmail.com>
2022-01-07 10:16:51 +0700

    Create entity class representative table "NhanVien"

---
+++ src/Entities/NhanVien.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+/**
+ *
+ * @author Phat
+ */
+public class NhanVien {
+    private String maNhanVien;
+
+    public String getMaNhanVien() {
+        return maNhanVien;
+    }
+
+    public void setMaNhanVien(String maNhanVien) {
+        this.maNhanVien = maNhanVien;
+    }
+}
